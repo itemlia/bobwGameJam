11e9c16e979f23f1af2d3a254a3adf519f0c9487
lia harakis <lia.harakis@yahoo.com>
2025-08-28 16:39:51 +0100

    asteroid spawner done

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] private Transform end;
+    [SerializeField] private Transform tPlayer;
+
+    [SerializeField] private float timer;
+    [SerializeField] private float offset;
+
+    [SerializeField] private GameObject asteroid;
+
+    [SerializeField] private bool positionReached;
+
+    private float spawnPos;
+
+
+    private IEnumerator dropTimer()
+    {
+        while (tPlayer.position.y <= end.position.y)
+        {
+            yield return new WaitForSeconds(timer);
+            Instantiate(asteroid, new Vector3(randomSpawn(), end.position.y, 0f), Quaternion.identity);
+        }
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(dropTimer());
+    }
+
+    private float randomSpawn()
+    {
+        spawnPos = Random.Range(end.position.x, end.position.x + offset);
+
+        return spawnPos;
+    }
+
+}
